733510329e890ae0a10e35a30e29e70a9bfc9fe9
QUY666666 <lenhatquy2401@gmail.com>
2024-12-13 02:37:11 +0700

    update 13/12 test3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
+    handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
