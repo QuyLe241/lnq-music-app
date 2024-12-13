6e5a5ee4d26f9afc0bdf9f00a10576607a5f9322
QUY666666 <lenhatquy2401@gmail.com>
2024-12-14 03:42:28 +0700

    14/12 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Header from "./Components/Header/Header";
 import Sibar from "./Components/Sibar/Sibar";
-import Body from "./Components/Body/Body";
+// import Body from "./Components/Body/Body";
 import Footer from "./Components/Footer/Footer";
 import { store } from "./reudx/store";
 import { Provider } from "react-redux";
